8c847560e82f87331a287e5aba16eaa4293453f4
rodri <25470856+drigohhh@users.noreply.github.com>
2024-09-30 17:13:32 -0300

    [Drigo] Total aplicação da LoginPage com rotas e temas

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,6 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:jarvis_assistent/Themes/themes.dart';
+import 'package:jarvis_assistent/main_screen.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -19,34 +18,50 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Padding(
-              padding: const EdgeInsetsDirectional.fromSTEB(24, 20, 24, 0),
-              child: Column(
-                children: [
-                  _titleCard(),
-                  _emailInput(),
-                  _senhaInput(),
-                  _loginButtons(),
-                  _createAccountButton()
-                ],
-              )
-            )
-          ],
+        appBar: _buildAppBar(),
+        body: SizedBox.expand(
+          child: Container(
+            padding: const EdgeInsetsDirectional.fromSTEB(24, 20, 24, 0),
+            color: AppTheme.primaryColor,
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                _fullNameInput(),
+                _emailInput(),
+                _senhaInput(),
+                _loginButtons(),
+                _googleButton(),
+                _createAccountButton(),
+              ],
+            ),
+          ),
         ),
-      )
+      ),
     );
   }
 
-  Widget _titleCard() {
-    return const Padding(
-      padding: EdgeInsetsDirectional.fromSTEB(100, 0, 100, 0),
-      child: Text(
-        "Jarvis Assistant",
-        style: TextStyle(fontSize: 32),
-      ),
+  Widget _fullNameInput() {
+    return Padding( // Input de Email
+      padding: const EdgeInsetsDirectional.fromSTEB(100, 20, 100, 0),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        style: TextStyle(
+          fontSize: 18,
+          color: AppTheme.textColor,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 1.2
+        ),
+        decoration: InputDecoration(
+          labelText: "Nome completo",
+          labelStyle: TextStyle(color: AppTheme.textColor),
+          hintText: "Digite seu nome",
+          hintStyle: TextStyle(color: AppTheme.textColor),
+          enabledBorder: AppTheme.outlineInputBorder,
+          fillColor: AppTheme.appBarColor,
+          focusedBorder: AppTheme.outlineInputBorder,
+          filled: true,
+        ),
+      )
     );
   }
 
@@ -63,7 +78,7 @@ class _LoginPageState extends State<LoginPage> {
         ),
         decoration: InputDecoration(
           labelText: "Email",
-          labelStyle: const TextStyle(fontSize: 18),
+          labelStyle: TextStyle(color: AppTheme.textColor),
           hintText: "Digite seu E-mail",
           hintStyle: TextStyle(color: AppTheme.textColor),
           enabledBorder: AppTheme.outlineInputBorder,
@@ -78,33 +93,67 @@ class _LoginPageState extends State<LoginPage> {
   Widget _senhaInput(){
     return Padding( // Input de Senha
       padding: const EdgeInsetsDirectional.fromSTEB(100, 20, 100, 0),
-      child: TextFormField(
+      child: TextField(
         obscureText: true,
         enableSuggestions: false,
         autocorrect: false,
+        style: TextStyle(
+          fontSize: 18,
+          color: AppTheme.textColor,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 1.2
+        ),
         decoration: InputDecoration(
           labelText: "Senha",
-          labelStyle: const TextStyle(fontSize: 18),
-          hintText: "Digite sua Senha",
-          hintStyle: const TextStyle(fontSize: 18),
-          enabledBorder: OutlineInputBorder(
-            borderSide: const BorderSide(
-              color: Color.fromARGB(255, 105, 7, 0),
-              width: 1.0,
-            ),
-            borderRadius: BorderRadius.circular(radius),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: const BorderSide(
-              color: Color.fromARGB(255, 105, 7, 0),
-              width: 1.0,
-            ),
-            borderRadius: BorderRadius.circular(radius)
-          )
+          labelStyle: TextStyle(color: AppTheme.textColor),
+          hintText: "Digite sua senha",
+          hintStyle: TextStyle(color: AppTheme.textColor),
+          enabledBorder: AppTheme.outlineInputBorder,
+          fillColor: AppTheme.appBarColor,
+          focusedBorder: AppTheme.outlineInputBorder,
+          filled: true,
         ),
       )
     );
+  }
 
+  Widget _googleButton(){
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 24),
+          child: TextButton(
+            onPressed: () {
+              Navigator.push(
+                context, 
+                MaterialPageRoute(builder: (context) => Mainscreen())
+              );
+            },
+            style: ButtonStyle(
+              backgroundColor: WidgetStateProperty.all<Color>(const Color(0xff9489F5)),
+              foregroundColor: WidgetStateProperty.all<Color>(AppTheme.textColor),
+              minimumSize: WidgetStateProperty.all<Size>(const Size(300, 60)),
+            ),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "G",
+                  style: TextStyle(fontSize: 22)
+                ),
+                SizedBox(width: 12), // Espaço entre o ícone e o texto
+                Text(
+                  "Entrar com o Google",
+                  style: TextStyle(fontSize: 22),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
   }
 
   Widget _loginButtons(){
@@ -115,32 +164,33 @@ class _LoginPageState extends State<LoginPage> {
         Padding(
           padding: const EdgeInsetsDirectional.fromSTEB(0, 24, 0, 24),
           child: TextButton(
-            onPressed: () {},
+            onPressed: () { /* ! Funcionalidade de criar conta aqui */ },
             style: ButtonStyle(
-              foregroundColor: MaterialStateProperty.all<Color>(Colors.pink),
-              minimumSize: MaterialStateProperty.all<Size>(const Size(120, 50)),
+              foregroundColor: WidgetStateProperty.all<Color>(Colors.pink),
+              minimumSize: WidgetStateProperty.all<Size>(const Size(120, 50)),
             ),
-            child: const Text(
-              "Criar conta",
-              style: TextStyle(fontSize: 22),
+            child: Text(
+              "Esqueceu a senha?",
+              style: TextStyle(
+                fontSize: 22,
+                color: AppTheme.textColor, 
+              ),
             ),
           ),
         ),
         Padding(
           padding: const EdgeInsetsDirectional.fromSTEB(0, 24, 0, 24),
           child: TextButton(
-            onPressed: () {},
+            onPressed: () {
+              Navigator.push(
+                context, 
+                MaterialPageRoute(builder: (context) => Mainscreen())
+              );
+            },
             style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all<Color>(Colors.pink),
-              foregroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 255, 255, 255)),
-              overlayColor: MaterialStateProperty.resolveWith((states) {
-                if (states.contains(MaterialState.hovered)) return const Color.fromARGB(255, 233, 30, 30);
-                if (states.contains(MaterialState.focused) || states.contains(MaterialState.pressed)) {
-                  return const Color.fromARGB(255, 243, 33, 33).withOpacity(0.12);
-                }
-                return Colors.pink; 
-              }),
-              minimumSize: MaterialStateProperty.all<Size>(const Size(120, 50)),
+              backgroundColor: WidgetStateProperty.all<Color>(const Color(0xff9489F5)),
+              foregroundColor: WidgetStateProperty.all<Color>(AppTheme.textColor),
+              minimumSize: WidgetStateProperty.all<Size>(const Size(120, 50)),
             ),
             child: const Text(
               "Entrar",
@@ -161,14 +211,35 @@ class _LoginPageState extends State<LoginPage> {
           padding: EdgeInsetsDirectional.fromSTEB(24, 0, 4, 0),
           child: Text(
             "Criar conta",
-            style: TextStyle(fontSize: 22),
+            style: TextStyle(
+              fontSize: 22,
+              color: Color(0xff9489F5), 
+            ),
           )
         ),
         Padding(
           padding: EdgeInsetsDirectional.fromSTEB(10, 0, 4, 0),
-          child: Icon(Icons.arrow_forward)
+          child: Icon(
+            Icons.arrow_forward,
+            color: Color(0xff9489F5),
+          )
         )
       ]
+    );
+  }
+
+  PreferredSizeWidget _buildAppBar() {
+    return AppBar(
+      backgroundColor: AppTheme.appBarColor,
+      centerTitle: true,
+      title: Text(
+        'Jarvis Assistant',
+        style:
+            TextStyle(color: AppTheme.textColor, fontWeight: FontWeight.bold),
+      ),
+      iconTheme: IconThemeData(
+        color: AppTheme.secondaryColor
+      ),
     );
   }
 }
